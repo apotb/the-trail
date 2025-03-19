0257b4b0f0291c3880fcdedddbea4df44f384f93
apotb <endroo04@gmail.com>
2025-03-18 21:10:50 -0400

    moved shadow sprite up 4 pixels to start under feet

--- js/rpg_sprites.js
+++ js/rpg_sprites.js
@@ -703,6 +703,7 @@ Sprite_Actor.prototype.createShadowSprite = function() {
     this._shadowSprite.scale.y = -0.5;
     this._shadowSprite.tint = 0x000000;
     this._shadowSprite.alpha = 0.5;
+    this._shadowSprite.y = -4;
     this._shadowSprite.z = 0.99;
     this.addChildAt(this._shadowSprite, this.children.indexOf(this._mainSprite) + 1);
 };
