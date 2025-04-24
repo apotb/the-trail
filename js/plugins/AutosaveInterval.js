dea9a39f96a24b199693d8a8fbed37b9790db7ce
apotb <endroo04@gmail.com>
2025-04-24 14:43:47 -0400

    changing autosave interval resets countdown
    fixes bug where if you set it to 30 minutes then back down to 1 minute, it would wait 30 minutes before beginning to autosave again

--- js/plugins/AutosaveInterval.js
+++ js/plugins/AutosaveInterval.js
@@ -1,10 +1,14 @@
 Game_System.prototype.autosaveInterval = function() {
-    return [0, 1, 2, 3, 5, 10, 15, 30];
+  return [0, 1, 2, 3, 5, 10, 15, 30];
 };
 
 Game_System.prototype.getAutosaveInterval = function() {
-    return this.autosaveInterval()[ConfigManager.autosaveInterval];
-}
+  return this.autosaveInterval()[ConfigManager.autosaveInterval];
+};
+
+Game_System.prototype.setAutosaveInterval = function(minutes) {
+  this._secUntilAutosave = minutes * 60;
+};
 
 ___Window_Options__prototype__statusText___ = Window_Options.prototype.statusText;
 Window_Options.prototype.statusText = function(index) {
@@ -15,5 +19,13 @@ Window_Options.prototype.statusText = function(index) {
     return $gameSystem.autosaveInterval()[value] + ' minute' + (value > 1 ? 's' : '');
   } else {
     return ___Window_Options__prototype__statusText___.call(this, index);
+  }
+};
+
+___Window_Options__prototype__changeValue___ = Window_Options.prototype.changeValue;
+Window_Options.prototype.changeValue = function(symbol, value) {
+  ___Window_Options__prototype__changeValue___.call(this, symbol, value);
+  if (symbol === 'autosaveInterval') {
+    $gameSystem.setAutosaveInterval($gameSystem.getAutosaveInterval());
   }
 };
