7dafb2358ce3149bfd93dfd15f51958d52bcb76d
AP <endroo04@gmail.com>
2026-03-04 19:34:54 -0500

    hopefully fixed deploy-all.bat

--- scripts/deployment/deploy-all.bat
+++ scripts/deployment/deploy-all.bat
@@ -1,13 +1,13 @@
 git switch master
 git pull
-npm run deploy
+call npm run deploy
 
 git switch demo
 git pull
-npm run deploy
+call npm run deploy
 
 git switch playtest
 git pull
-npm run deploy
+call npm run deploy
 
 git switch master
